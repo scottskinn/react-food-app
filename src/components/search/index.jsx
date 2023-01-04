47914a8ba86d0c779fdf6c19c4b4ef1466c0db01
scottskinn <84105761+scottskinn@users.noreply.github.com>
2023-01-04 16:19:24 -0500

    search function

--- src/components/search/index.jsx
+++ src/components/search/index.jsx
@@ -1,9 +1,33 @@
+import { useState } from 'react';
 import './style.css'
 
-const Search = () => {
+
+
+const Search = (props) => {
+    const {getDataFromSearchComponent} = props;
+
+    const [inputValue, setInputValue] = useState('')
+
+    const handleInputValue = (event) => {
+        const {value} = event.target;
+        setInputValue(value)
+    }
+    console.log(inputValue)
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        getDataFromSearchComponent(inputValue)
+    }
+
     return (
-        <form className="search">
-            <input name='search' placeholder="Search Recipes" id='search-input' />
+        <form onSubmit={handleSubmit} className="search">
+            <input 
+                name='search' 
+                value={inputValue} 
+                onChange={handleInputValue} 
+                placeholder="Search Recipes" 
+                id='search-input' 
+            />
             <button type="submit">Search</button>
         </form>
     )
--- src/pages/homepage/index.jsx
+++ src/pages/homepage/index.jsx
@@ -1,9 +1,15 @@
 import Search from "../../components/search";
 
 const Homepage = () => {
+
+    // this gets data from the search props
+    const getDataFromSearchComponent = (getDataFromSearchComponent) => {
+        console.log(getDataFromSearchComponent, 'getData')
+    }
+
     return (
         <div className="homepage">
-            <Search />
+            <Search getDataFromSearchComponent = {getDataFromSearchComponent} />
         </div>
     )
 }
